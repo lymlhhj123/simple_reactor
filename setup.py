005b5e078fab525d56eb44742d0d692a28149454
jun hu <524964426@qq.com>
2023-02-03 23:02:18 +0800

    add version

--- setup.py
+++ setup.py
@@ -3,12 +3,20 @@
 from setuptools import setup, find_packages
 
 
+def get_version():
+    """
+
+    :return:
+    """
+    return open("version").readline().strip()
+
+
 setup(
     zip_safe=False,
     include_package_data=True,
     name="libreactor",
     description="io notify based on reactor mode",
-    version="1.0.0",
+    version=get_version(),
     author="Jun Hu",
     author_email="",
     packages=find_packages(exclude=["example"]),
@@ -20,6 +28,9 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
         'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
 )
