581ec122c6086e2586f44dc212a622a4c5ab87e3
jun hu <524964426@qq.com>
2023-02-04 10:01:41 +0800

    fix bug

--- example/msg_based/msg_client.py
+++ example/msg_based/msg_client.py
@@ -1,5 +1,7 @@
 # coding: utf-8
 
+import json
+
 from libreactor import Context
 from libreactor import StreamReceiver
 from libreactor import EventLoop
@@ -8,56 +10,14 @@ from libreactor import TcpClient
 
 class MyProtocol(StreamReceiver):
 
-    def __init__(self):
-
-        super(MyProtocol, self).__init__()
-        self.ops = 0
-        self._end = False
-        self._timer = None
-
     def msg_received(self, msg):
         """
 
         :param msg:
         :return:
         """
-        if self._end:
-            self._timer = None
-            self.close_connection()
-            return
-
-        self.ops += 1
-        self.send_msg(msg)
-
-    def connection_error(self, error):
-        """
-
-        :param error:
-        :return:
-        """
-        if self._timer:
-            self._timer.cancel()
-            self._timer = None
-
-    def start_test(self):
-        """
-
-        :return:
-        """
-        start_time = self.event_loop.time()
-
-        self._timer = self.event_loop.call_later(60, self._count_down, start_time)
-
-    def _count_down(self, start_time):
-        """
-
-        :param start_time:
-        :return:
-        """
-        end_time = self.event_loop.time()
-        self._end = True
-        ops = self.ops / (end_time - start_time)
-        print(f"ops: {ops}")
+        print(f"{msg}")
+        self.event_loop.call_later(5, self.send_msg, msg)
 
 
 class MyContext(Context):
@@ -71,9 +31,9 @@ def on_established(protocol):
     :param protocol:
     :return:
     """
-    protocol.start_test()
-
-    protocol.send_msg(b"hello")
+    user_data = {"1": "2", "3": "4"}
+    msg = json.dumps(user_data).encode("utf-8")
+    protocol.send_msg(msg)
 
 
 ev = EventLoop()
