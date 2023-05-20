14105b22e540108349d27bad1c17cc4be0f4ba09
jun hu <524964426@qq.com>
2023-05-20 19:05:54 +0800

    update example

--- example/timing_wheel/test.py
+++ example/timing_wheel/test.py
@@ -11,9 +11,16 @@ scheduler = TimerScheduler(ev)
 
 def func():
 
-    print("test")
+    print("test", ev.time())
+
+
+def func1():
+
+    print("test1", ev.time())
 
 
 scheduler.call_later(5, func)
+
+scheduler.call_later(10, func1)
 
 ev.loop()
