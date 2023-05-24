b611d51b4dca33fc64bae89c3cb85a6151827e65
jun hu <524964426@qq.com>
2023-05-24 21:27:56 +0800

    fix bug

--- libreactor/timing_wheel/timing_wheel.py
+++ libreactor/timing_wheel/timing_wheel.py
@@ -33,7 +33,7 @@ class TimingWheel(object):
             virtual_id = int(timer.expiration // self.tick_ms)
             bucket = self.buckets[virtual_id % self.wheel_size]
             bucket.add_timer(timer)
-            result = bucket.set_expiration(timer.expiration)
+            result = bucket.set_expiration(virtual_id * self.tick_ms)
             return bucket, result
         else:
             if not self.overflow_timingwheel:
